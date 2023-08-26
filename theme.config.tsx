d7311ba5150d6cfa7e6c18c966667b2b54770c95
prime <105910256+PrimeTDMomega@users.noreply.github.com>
2023-08-26 21:31:12 +0530

    changed link placeholders to elara repo

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>ElaraLang</span>,
   project: {
-    link: 'https://github.com/PrimeTDMomega/ElaraLang-Docs/',
+    link: 'https://github.com/ElaraLang/elara/',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/PrimeTDMomega/ElaraLang-Docs/',
+  docsRepositoryBase: 'https://github.com/ElaraLang/elara/',
   footer: {
     text: 'Nextra Docs Template',
   },
