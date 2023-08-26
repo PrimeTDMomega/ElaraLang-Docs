d7dd8d0c1bc45e429842b1ab57ed776623b4c137
prime <105910256+PrimeTDMomega@users.noreply.github.com>
2023-08-26 15:42:49 +0000

    just dont ask me

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>ElaraLang</span>,
   project: {
-    link: 'https://github.com/PrimeTDMomega/capyAPI-docs',
+    link: 'https://github.com/PrimeTDMomega/ElaraLang-Docs/',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/PrimeTDMomega/capyAPI-docs',
+  docsRepositoryBase: 'https://github.com/PrimeTDMomega/ElaraLang-Docs/',
   footer: {
     text: 'Nextra Docs Template',
   },
